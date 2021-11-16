50067f9185f5fa8d94d96d8c6d20003a5996f6b0
Dayana Panova <dzopino@abv.bg>
2021-11-16 16:07:49 +0200

    Creating the findItem and the edit task functions.

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { TaskListContext } from '../context/TaskListContext'
 
 const TaskForm = () => {
-    const [addTask, clearTasks] = useContext(TaskListContext);
+    const [addTask, clearTasks, editTask, editItem] = useContext(TaskListContext);
     const [title, setTitle] = useState('');
 
     const handleChange = (e) => {
@@ -11,15 +11,29 @@ const TaskForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        addTask(title)
-        setTitle('');
+        if (editItem === null) {
+            addTask(title)
+            setTitle('');
+        } else {
+            editTask(title, editItem.id);
+        }
     }
+    useEffect(() => {
+        if (editItem !== null) {
+            setTitle(editItem.title)
+        } else {
+            setTitle('');
+        }
+    }, [editItem])
 
     return (
         <form className="form" onSubmit={handleSubmit}>
             <input onChange={handleChange} value={title} type="text" className="task-input" placeholder="Add Task.." required />
             <div className="buttons">
-                <button type="submit" className="btn add-task-btn">Add Task</button>
+                <button type="submit"
+                    className="btn add-task-btn"
+                    {...editItem ? 'Edit' : 'Add'}
+                >Add Task</button>
                 <button onClick={clearTasks} type="submit" className="btn clear-btn">Clear</button>
             </div>
         </form>
--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -10,19 +10,41 @@ export const TaskListContextProvider = (props) => {
         { title: 'Walk the dog', id: '3' },
     ])
 
+    const [editItem, setEditItem] = useState(null);
+
     const addTask = (title) => {
         setTasks([...tasks, { title, id: uuid() }])
     }
 
     const removeTask = (id) => {
-        setTasks(tasks.filter(task => task.id !== id))
+        setTasks(tasks.filter(task => task.id !== id));
     }
 
     const clearTasks = () => {
-        setTasks([])
+        setTasks([]);
     }
+
+    const editTask = (title, id) => {
+        const newTask = tasks.map(task => task.id === id ? { title, id } : task)
+        setTasks(newTask)
+        setEditItem(null);
+    }
+
+    const findItem = (id) => {
+        const item = tasks.find(task => task.id === id);
+        setEditItem(item);
+    }
+
     return (
-        <TaskListContext.Provider value={tasks, addTask, removeTask, clearTasks}>
+        <TaskListContext.Provider value={
+            tasks,
+            addTask,
+            removeTask,
+            clearTasks,
+            findItem,
+            editItem,
+            editTask
+        }>
             {props.children}
         </TaskListContext.Provider>
     )
